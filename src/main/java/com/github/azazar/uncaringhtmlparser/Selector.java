305a18b924c09ae6a379f0ba2ceac83006deb67c
Mikhail Yevchenko <m..@uo1.net>
2022-02-08 02:08:42 +0500

    Exclude dot from allowed tag chars (breaks valid xml tags support)

--- src/main/java/com/github/azazar/uncaringhtmlparser/Selector.java
+++ src/main/java/com/github/azazar/uncaringhtmlparser/Selector.java
@@ -35,7 +35,7 @@ public class Selector {
         if (index == 0)
             return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_';
 
-        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' || c == '-' || c == '.';
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' || c == '-';
     }
     
     private static Stream<HtmlElement> queryAll(List<String> selector, Stream<Html> htmlStream) {
